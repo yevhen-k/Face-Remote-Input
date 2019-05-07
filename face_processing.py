3c71667b0e71e8e689cfcf1669b21b04bb55237f
krasnokutskyi (Євген Краснокутський) <krasnokutskyi@office.local>
2019-05-07 16:46:33 +0300

    mouse move and left mouse click implemented

--- face_processing.py
+++ face_processing.py
@@ -1,11 +1,13 @@
 import numpy as np
+import dlib
+import cv2
+import pyautogui
 
 
 RIGHT_EYE = list(i for i in range(36, 42))
 LEFT_EYE = list(i for i in range(42, 48))
 NOSE_BRIDGE = list(i for i in range(27, 31))
 LIPS = list(i for i in range(60, 68))
-
 
 def landmark_to_np(shape, dtype="int"):
     # initialize the list of (x, y)-coordinates
@@ -68,22 +70,67 @@ def face_end(landmarks_np):
 
 
 def up_down_direction(landmarks_np):
-    '''Value 3 means defaul position of face.
-    Value > 3 means pointing down.
-    Value < 3 means pointing up.
-    Delimeter 10 means noise suppression
+    '''Value 0 means defaul position of face.
+    Value > 0 means pointing down.
+    Value < 0 means pointing up.
     '''
-    vartical_level = int((face_start(landmarks_np)[1] + face_end(landmarks_np)[1]) / 2)
+    upper_vartical_level = int((face_start(landmarks_np)[1] + face_end(landmarks_np)[1]) / 2)
+    lower_vartical_level = int((landmarks_np[4,1] + landmarks_np[12,1]) / 2)
     nose_tip_pos = nose_tip(landmarks_np)[1]
-    return int((nose_tip_pos - vartical_level) / 10)
+    upper_vl_to_nose_distance = upper_vartical_level - nose_tip_pos
+    lower_vl_to_nose_distance = nose_tip_pos - lower_vartical_level
+    res = round((upper_vl_to_nose_distance / lower_vl_to_nose_distance * 10) / 6 - 1.3, 1)
+    if -0.5 < res < 0.5:
+        return 0
+    else:
+        return res
 
 
 def left_right_direction(landmarks_np):
-    '''Value -2 <= x <= 2 means face is centered
-    value < -2 means face is turned right
-    value > 2 means face is turned left
+    '''Value x == 0 means face is centered
+    value < 0 means face is turned right
+    value > 0 means face is turned left
     '''
     nose_tip_pos = nose_tip(landmarks_np)[0]
     nose_to_fase_start_distance = nose_tip_pos - face_start(landmarks_np)[0]
     nose_to_fase_end_distance = face_end(landmarks_np)[0] - nose_tip_pos
-    return int((nose_to_fase_start_distance - nose_to_fase_end_distance) / 10)
+    res = round((nose_to_fase_start_distance / nose_to_fase_end_distance * 10) / 20 - 0.5, 1)
+    if -0.2 < res < 0.2:
+        return 0
+    else:
+        return res
+
+
+def display_faces(faces, frame, gray, face_landmark_predictor):
+    if not len(faces):
+        return
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        left = x
+        top = y
+        right = x + w 
+        bottom = y + h
+        dlib_rectangle = dlib.rectangle(left, top, right, bottom)
+        landmarks = face_landmark_predictor(gray, dlib_rectangle)
+        landmarks_np = landmark_to_np(landmarks)
+
+        mr = mouth_ratio(landmarks_np)
+        mo = is_mouth_opened(landmarks_np)
+        up_down = up_down_direction(landmarks_np)
+        left_right = left_right_direction(landmarks_np)
+
+        pyautogui.moveRel(-50 * left_right, 10 * up_down, duration=0.01)
+        if mo:
+            pyautogui.click()
+
+        cv2.putText(frame, 'mouth ratio = {}'.format(mr), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        cv2.putText(frame, 'mouth opened = {}'.format(mo), (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        cv2.putText(frame, 'up/down direction = {}'.format(up_down), (10,120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+        cv2.putText(frame, 'left/right direction = {}'.format(left_right), (10,160), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+
+        for idx, (x, y) in enumerate(landmarks_np):
+            if idx in [36,39,27,30,42,45]:
+                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            else:
+                cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
--- main.py
+++ main.py
@@ -24,30 +24,7 @@ def main():
 
         # Get all faces on gray frame
         faces = face_cascade_detector.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
-            left = x
-            top = y
-            right = x + w 
-            bottom = y + h
-            dlib_rectangle = dlib.rectangle(left, top, right, bottom)
-            landmarks = face_landmark_predictor(gray, dlib_rectangle)
-            landmarks_np = landmark_to_np(landmarks)
-
-            mr = mouth_ratio(landmarks_np)
-            mo = is_mouth_opened(landmarks_np)
-            up_down = up_down_direction(landmarks_np)
-            left_right = left_right_direction(landmarks_np)
-            cv2.putText(frame, 'mouth ratio = {}'.format(mr), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-            cv2.putText(frame, 'mouth opened = {}'.format(mo), (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-            cv2.putText(frame, 'up/down direction = {}'.format(up_down), (10,120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-            cv2.putText(frame, 'left/right direction = {}'.format(left_right), (10,160), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-
-            for idx, (x, y) in enumerate(landmarks_np):
-                if idx in [36,39,27,30,42,45]:
-                    cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-                else:
-                    cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+        display_faces(faces, frame, gray, face_landmark_predictor)
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
